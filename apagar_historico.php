3fdc5a21ba526e774acd2d07d38eada91204baac
waltermoorenix <vlolxd@gmail.com>
2025-10-02 00:25:07 +0100

    Update apagar_historico.php

--- apagar_historico.php
+++ apagar_historico.php
@@ -1,8 +1,8 @@
 <?php
 // Dados reais do teu alojamento
-$host = "tb-be04-linweb086.srv.teamblue-ops.net";
-$user = "cybewo_cyberstationpt";          // o teu utilizador MySQL
-$password = "Vl1991239!";                 // a tua password MySQL
+$host = "";
+$user = "";          // o teu utilizador MySQL
+$password = "";                 // a tua password MySQL
 $dbname = "cybewo_historico";             // substitui pelo nome exato da base de dados
 
 $conn = new mysqli($host, $user, $password, $dbname);
