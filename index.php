92bf456db6f3b784c714dd83e63fde027bf3d4cc
waltermoorenix <vlolxd@gmail.com>
2025-10-02 00:26:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Dados reais do teu alojamento
-$host = "tb-be04-linweb086.srv.teamblue-ops.net";
-$user = "cybewo_cyberstationpt";          // o teu utilizador MySQL
-$password = "Vl1991239!";                 // a tua password MySQL
+$host = "";
+$user = "";          // o teu utilizador MySQL
+$password = "";                 // a tua password MySQL
 $dbname = "cybewo_historico";             // substitui pelo nome exato da base de dados
 
 $conn = new mysqli($host, $user, $password, $dbname);
