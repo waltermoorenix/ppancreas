7d528ddf144a320e49dd5477a1539b9ff7cda197
waltermoorenix <vlolxd@gmail.com>
2025-10-07 12:26:21 +0100

    Add files via upload
    listing by pages, max 10 results

--- listar_historico.php
+++ listar_historico.php
@@ -1,9 +1,9 @@
+
 <?php
-// Dados reais do teu alojamento
-$host = "";
-$user = "";          // o teu utilizador MySQL
-$password = "";                 // a tua password MySQL
-$dbname = "cybewo_historico";             // substitui pelo nome exato da base de dados
+$host = "tb-be04-linweb086.srv.teamblue-ops.net";
+$user = "cybewo_cyberstationpt";          
+$password = "Vl1991239!";                
+$dbname = "cybewo_historico";            
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -11,7 +11,14 @@ if ($conn->connect_error) {
   die("Erro de ligação: " . $conn->connect_error);
 }
 
-$result = $conn->query("SELECT * FROM historico ORDER BY data_hora DESC");
+// Parâmetros de paginação
+$pagina = isset($_GET['pagina']) ? intval($_GET['pagina']) : 1;
+$limite = isset($_GET['limite']) ? intval($_GET['limite']) : 10;
+$offset = ($pagina - 1) * $limite;
+
+// Consulta paginada
+$sql = "SELECT * FROM historico ORDER BY data_hora DESC LIMIT $limite OFFSET $offset";
+$result = $conn->query($sql);
 
 $historico = [];
 while ($row = $result->fetch_assoc()) {
